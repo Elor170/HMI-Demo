4426b7c8ba5d2cf24245c7d457cc7dce71ee8ad2
Orche <orkv0099@gmail.com>
2024-06-18 15:20:14 +0300

    Finished basic video player

--- hmi-frontend/src/Pages/StreamingPage/StreamingPage.tsx
+++ hmi-frontend/src/Pages/StreamingPage/StreamingPage.tsx
@@ -3,11 +3,14 @@ import { useRef, useState } from "react";
 import "./StreamingPage.scss";
 import VideoController from "./VideoController";
 import ReactPlayer from "react-player";
+import { Slider, CircularProgress } from "@mui/material";
 
 export default function StreamingPage() {
   const [resolution, setResolution] = useState<string>("4k");
   const [timeStamp, setTimeStamp] = useState(0);
   const [isPlaying, setIsPlaying] = useState(true);
+  const [duration, setDuration] = useState(0);
+  const [isBuffering, setIsBuffering] = useState(true);
   const videoUrl = `${STREAMER_SERVER}/video/${resolution}`;
 
   const videoRef = useRef<ReactPlayer>(null);
@@ -26,16 +29,39 @@ export default function StreamingPage() {
     }
   };
 
+  const onSliderMove = (_: Event, newTimeStamp: number | number[]) => {
+    setTimeStamp(newTimeStamp as number);
+    if (videoRef.current) {
+      videoRef.current.seekTo(newTimeStamp as number);
+    }
+  };
+
   return (
     <div className="streaming-page">
-      <div className="video-container">
-        <ReactPlayer
-          style={{ width: "100%" }}
-          ref={videoRef}
-          playing={isPlaying}
-          onStart={onVideoStart}
-          url={videoUrl}
-        />
+      <div>
+        <div className="video-container">
+          {isBuffering && <CircularProgress className="video-loading" />}
+          <div
+            className={`react-player ${
+              isBuffering ? "react-player-loading" : ""
+            }`}
+          >
+            <ReactPlayer
+              width="100%"
+              ref={videoRef}
+              playing={isPlaying}
+              onStart={onVideoStart}
+              url={videoUrl}
+              onDuration={(newDuration) => setDuration(newDuration)}
+              onProgress={(state) => setTimeStamp(state.playedSeconds)}
+              onBuffer={() => setIsBuffering(true)}
+              onBufferEnd={() => setIsBuffering(false)}
+              loop
+            />
+          </div>
+        </div>
+
+        <Slider value={timeStamp} max={duration} onChange={onSliderMove} />
 
         <VideoController
           videoRef={videoRef}
@@ -44,7 +70,6 @@ export default function StreamingPage() {
           setIsPlaying={setIsPlaying}
         />
       </div>
-
       <span style={{ userSelect: "none" }}>
         Current resolution: {resolution}
       </span>
