a736351109eaec2d012914129017dcb28706d926
Orche <orkv0099@gmail.com>
2024-06-18 15:35:04 +0300

    Added option for fullscreen

--- hmi-frontend/src/Pages/StreamingPage/VideoController.tsx
+++ hmi-frontend/src/Pages/StreamingPage/VideoController.tsx
@@ -2,7 +2,9 @@ import { Box, IconButton, MenuItem, MenuList, Paper } from "@mui/material";
 import ResolutionSelector from "./ResolutionSelector";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import PauseIcon from "@mui/icons-material/Pause";
+import FullscreenIcon from "@mui/icons-material/Fullscreen";
 import ReactPlayer from "react-player";
+import { useState } from "react";
 
 interface VideoControllerProps {
   setResolution: (newResolution: string) => void;
@@ -15,18 +17,52 @@ export default function VideoController({
   setResolution,
   isPlaying,
   setIsPlaying,
+  videoRef,
 }: VideoControllerProps) {
+  const [fullScreen, setFullScreen] = useState(false);
+
+  const handleFullScreen = () => {
+    if (!fullScreen) {
+      // Enter fullscreen
+      if (videoRef.current) {
+        const player = videoRef.current.getInternalPlayer();
+        if (player) {
+          if (player.requestFullscreen) {
+            player.requestFullscreen();
+          } else if (player.webkitRequestFullscreen) {
+            /* Safari */
+            player.webkitRequestFullscreen();
+          } else if (player.msRequestFullscreen) {
+            /* IE11 */
+            player.msRequestFullscreen();
+          }
+          setFullScreen(true);
+        }
+      }
+    } else {
+      // Exit fullscreen
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      }
+      setFullScreen(false);
+    }
+  };
+
   return (
     <Box sx={{ width: "100%" }} component={Paper}>
       <MenuList sx={{ display: "flex", flexDirection: "row" }}>
-        <MenuItem>
-          <IconButton onClick={() => setIsPlaying(!isPlaying)}>
+        <MenuItem onClick={() => setIsPlaying(!isPlaying)}>
+          <IconButton>
             {isPlaying ? <PlayArrowIcon /> : <PauseIcon />}
           </IconButton>
         </MenuItem>
 
-        <MenuItem>
-          <ResolutionSelector setResolution={setResolution} />
+        <ResolutionSelector setResolution={setResolution} />
+
+        <MenuItem onClick={() => handleFullScreen}>
+          <IconButton>
+            <FullscreenIcon />
+          </IconButton>
         </MenuItem>
       </MenuList>
     </Box>
