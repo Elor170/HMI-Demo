3a9c82a2b5259b44d4f3d795e021d31765194a18
Elor Ifrach <elor170@gmail.com>
2024-06-19 12:03:20 +0300

    Fix PR comments
    prevent some of the regular behavior of mouse buttons

--- hmi-frontend/src/Pages/IOChecks/MouseTester/MouseTester.tsx
+++ hmi-frontend/src/Pages/IOChecks/MouseTester/MouseTester.tsx
@@ -62,6 +62,7 @@ export default function MouseTester() {
         break;
       case 1:
         clickEvent = 'scrollClicked';
+        event.preventDefault();
         break;
       case 2:
         clickEvent = 'rightClicked';
@@ -76,7 +77,8 @@ export default function MouseTester() {
   };
 
   const handleReset = () => setChecksList(initialCheckList);
-  
+  const handleContextMenu = (event: React.MouseEvent) => event.preventDefault();
+
   useEffect(() => {
       const scrollContainer = containerRef.current;
       if (scrollContainer) { 
@@ -91,24 +93,26 @@ export default function MouseTester() {
   }, []);
   
   return (
-    <div className={styles.mouseTester_container} ref={containerRef}>
-      <div className={styles.mouseTester}>
-        <MouseView buttonsStatus={checkList}/>
+    <div className={styles.mouseTester_container} ref={containerRef} onContextMenu={handleContextMenu}>
+      <div className={styles.mouseTester_scrollArea}>
+        <div className={styles.mouseTester} >
+          <MouseView buttonsStatus={checkList}/>
 
-        {Object.keys(checkList).map((key: string) => {    
-          let checkTest = key[0].toUpperCase() + key.substring(1);
-          checkTest = checkTest.replace(/([a-z])([A-Z])/g, '$1 $2');
+          {Object.keys(checkList).map((key: string) => {    
+            let checkTest = key[0].toUpperCase() + key.substring(1);
+            checkTest = checkTest.replace(/([a-z])([A-Z])/g, '$1 $2');
 
-          const checkStatus = checkList[key as keyof ButtonCheckList];
+            const checkStatus = checkList[key as keyof ButtonCheckList];
 
-          return <div className={styles.mouseTester_item} key={key}>
-            <span>{checkTest}</span>
-            {(checkStatus ? <CheckBox className={styles.checkBoxIcon}/> : <CheckBoxOutlineBlank className={styles.checkBoxIcon}/>)}
-          </div>
-          })
-        }
-        
-        <button className={styles.resetButton} onClick={handleReset}>Reset Test</button>
+            return <div className={styles.mouseTester_item} key={key}>
+              <span>{checkTest}</span>
+              {(checkStatus ? <CheckBox className={styles.checkBoxIcon}/> : <CheckBoxOutlineBlank className={styles.checkBoxIcon}/>)}
+            </div>
+            })
+          }
+          
+          <button className={styles.resetButton} onClick={handleReset}>Reset Test</button>
+        </div>
       </div>
     </div>
   )
