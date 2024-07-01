ab6c8316595150cfc87d17df7d8b7fbb75484460
Elor Ifrach <elor170@gmail.com>
2024-07-01 21:54:43 +0300

    Add features:
    * reset keyboard test button
    * keyboard test progress
    * fix bugs

--- hmi-frontend/src/Pages/IOChecks/KeyboardTester/Keyboard/Keyboard.tsx
+++ hmi-frontend/src/Pages/IOChecks/KeyboardTester/Keyboard/Keyboard.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import styles from './Keyboard.module.scss';
 import { KeyboardLayout, layoutToInitState, KeyboardKey, specialKeysCodes } from './KeyboardLayouts';
 import KeyboardSection from './KeyboardSection';
+import LinearWithValueLabel from './LinearWithValueLabel';
 
 interface KeyboardProps {
   layout: KeyboardLayout;
@@ -10,6 +11,7 @@ interface KeyboardProps {
 
 export default function Keyboard ({layout, isVisible}: KeyboardProps) {
   const [keyboardStatus, setkeyboardStatus] = useState(layoutToInitState(layout));
+  const [keyboardPercentage, setKeyboardPercentage] = useState<number>(0);
   const flatLayout = [...layout.leftSection.flat(), ...layout.middleSection.flat(), ...layout.rightSection.flat()];
   
   const setKeyStatus = (key: string) => {
@@ -30,9 +32,12 @@ export default function Keyboard ({layout, isVisible}: KeyboardProps) {
         if (key !== keyObject.view) 
           return;
     }
-
-    setKeyStatus(event.code)
+    
+    if (isVisible)
+      setKeyStatus(event.code);
   }
+
+  const resetTest = () => setkeyboardStatus(layoutToInitState(layout));
 
   useEffect(() => {
     window.addEventListener('keydown', handleKeyDown);
@@ -40,14 +45,28 @@ export default function Keyboard ({layout, isVisible}: KeyboardProps) {
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     }
-  }, [keyboardStatus])
+  }, [keyboardStatus, isVisible]);
+
+  useEffect(()=> {
+    const keyboardStatusVals = Object.values(keyboardStatus);
+    const countTrue = keyboardStatusVals.filter(keyStatus => keyStatus).length;
+    const truePercentage = countTrue / keyboardStatusVals.length;
+    const formatedPercentage = Number.parseFloat((truePercentage * 100).toFixed(2));
+
+    setKeyboardPercentage(formatedPercentage);
+  }, [keyboardStatus]);
 
 
   return (
-    <div className={styles.keyboard} style={{display: isVisible ? 'flex' : 'none'}}>
-      <KeyboardSection layout={layout} keyboardStatus={keyboardStatus} section='leftSection'/>
-      <KeyboardSection layout={layout} keyboardStatus={keyboardStatus} section='middleSection'/>
-      <KeyboardSection layout={layout} keyboardStatus={keyboardStatus} section='rightSection'/>
+    <div className={styles.keyboard_container}>
+      <div className={styles.keyboard} style={{display: isVisible ? 'flex' : 'none'}}>
+        <LinearWithValueLabel value={keyboardPercentage}/>
+        <button className={styles.resetButton} onClick={resetTest}>Reset Test</button>
+
+        <KeyboardSection layout={layout} keyboardStatus={keyboardStatus} section='leftSection'/>
+        <KeyboardSection layout={layout} keyboardStatus={keyboardStatus} section='middleSection'/>
+        <KeyboardSection layout={layout} keyboardStatus={keyboardStatus} section='rightSection'/>
+      </div>
     </div>
   );
 };
