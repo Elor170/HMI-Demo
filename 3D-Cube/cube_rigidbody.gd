89ec67727ba9ca300c528d2a85a9a908c8698028
Orche <orkv0099@gmail.com>
2024-07-17 15:12:14 +0300

    migrated camera to first person

--- 3D-Cube/cube_rigidbody.gd
+++ 3D-Cube/cube_rigidbody.gd
@@ -1,7 +1,5 @@
 extends RigidBody3D
 
-var is_clicked = false
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,19 +8,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-func _input(event):
-	if event is InputEventMouseButton:
-		if event.pressed == true and event.button_index == 1:
-			self.position.x = event.position.x
-			self.position.y = event.position.y
-
-#func _input(event):
-	#if event is InputEventMouseButton:
-		#is_clicked = !is_clicked
-	#elif event is InputEventMouseMotion:
-		#self.position.x = event.position.x
-		#self.position.y = event.position.y
-
-   # Print the size of the viewport.
-	#print("Viewport Resolution is: ", get_viewport().get_visible_rect().size)
--- 3D-Cube/level.gd
+++ 3D-Cube/level.gd
@@ -1,5 +1,4 @@
 extends Node3D
-@onready var camera = $Camera3D
 
 func spawn_new_cube():
 	var new_rb = preload("res://cube_rigidbody.tscn").instantiate()
@@ -11,34 +10,3 @@ func spawn_new_cube():
 func _process(delta):
 	if Input.is_action_just_pressed("spawn_cube"):
 		spawn_new_cube()
-		
-	elif Input.is_action_pressed("move_left"):
-		camera.position.x += .5
-	elif Input.is_action_pressed("move_right"):
-		camera.position.x -= .5
-		
-	elif Input.is_action_pressed("move_forward"):
-		camera.position.z += .5
-		
-	elif Input.is_action_pressed("move_backwards"):
-		camera.position.z -= .5
-		
-	elif Input.is_action_pressed("move_up"):
-		camera.position.y += .5
-		
-	elif Input.is_action_pressed("move_down"):
-		camera.position.y -= .5
-		
-
-var rot_x = 0
-var rot_y = 0
-var LOOKAROUND_SPEED = .0001
-
-#func _input(event):
-	#if event is InputEventMouseMotion:
-		## modify accumulated mouse rotation
-		#rot_x += event.relative.x * LOOKAROUND_SPEED
-		#rot_y += event.relative.y * LOOKAROUND_SPEED
-		#transform.basis = Basis() # reset rotation
-		#camera.rotate_object_local(Vector3(0, 1, 0), rot_x) # first rotate in Y
-		#camera.rotate_object_local(Vector3(1, 0, 0), rot_y) # then rotate in X
