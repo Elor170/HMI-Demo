dd0f410e9d7ee6d70282e1ed4272b73f4747ba11
Orche <orkv0099@gmail.com>
2024-07-18 10:30:27 +0300

    locked mouse on character movement

--- 3D-Cube/player.gd
+++ 3D-Cube/player.gd
@@ -1,31 +1,34 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const SPEED = 5
 const JUMP_VELOCITY = 5
-const PUSH_FORCE = 50
+const PUSH_FORCE = 10 * SPEED
+const SENSITIVITY = .03
 
 @onready var neck := $Neck
 @onready var camera := $Neck/Camera3D
 
-func _unhandled_input(event):
-	if event is InputEventMouseButton:
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-		
-	elif event.is_action_pressed('ui_cancel'):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
-		if event is InputEventMouseMotion:
-			neck.rotate_y(-event.relative.x * .01)
-			camera.rotate_x(-event.relative.y * .01)
-			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event):		
+	if event is InputEventMouseMotion:
+		neck.rotate_y(-event.relative.x * SENSITIVITY)
+		camera.rotate_x(-event.relative.y * SENSITIVITY)			
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 func _physics_process(delta):
+	if move_and_slide():
+		for i in get_slide_collision_count():
+			var col = get_slide_collision(i)
+			if col.get_collider() is RigidBody3D:
+				col.get_collider().apply_force(col.get_normal() * -PUSH_FORCE)
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -42,13 +45,9 @@ func _physics_process(delta):
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = 0
+		velocity.z = 0
 
 	move_and_slide()
 	
-	if move_and_slide():
-		for i in get_slide_collision_count():
-			var col = get_slide_collision(i)
-			if col.get_collider() is RigidBody3D:
-				col.get_collider().apply_force(col.get_normal() * -PUSH_FORCE)
+
