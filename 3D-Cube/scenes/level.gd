1fd9dbb2c94c59783ee045d8d2f097e9d0fe4797
Orche <orkv0099@gmail.com>
2024-07-18 10:38:37 +0300

    fps counter

--- 3D-Cube/scenes/level.gd
+++ 3D-Cube/scenes/level.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 func spawn_new_cube():
-	var new_rb = preload("res://cube_rigidbody.tscn").instantiate()
+	var new_rb = preload("res://scenes/cube_rigidbody.tscn").instantiate()
 	new_rb.position.y = 15
 	new_rb.position.x = randf_range(-5, 5)
 	new_rb.position.z = randf_range(-5, 5)
