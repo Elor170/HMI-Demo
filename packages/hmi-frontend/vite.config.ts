6a1d27d0e40a0fa7d627d63cda7314be841d758c
Elor170 <58512741+Elor170@users.noreply.github.com>
2024-07-28 13:43:37 +0300

    Add Docker Dev Environment (#17)
    ##Add full docker Dev environment
    Use watch option to map and sync the code from the PC to the container
    Restart\Rebuild the services with watch option when needed
    Reorganised the file
    
    Commits:
    * Add Docker Dev Environment
    * Edit Project README
    * Remove the version from docker compose
    * Fix Frontend Port Bug

--- packages/hmi-frontend/vite.config.ts
+++ packages/hmi-frontend/vite.config.ts
@@ -1,18 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT: number = process.env.PORT as unknown as number;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  server: {
+    host: true,
+    port: PORT,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
     },
   },
-
   build: {
-    chunkSizeWarningLimit: Math.min(), // In JS, Math.min() = infinity, while Math.max() = -infinity... I sure do love JS!
+    chunkSizeWarningLimit: Infinity,
   },
+  preview: {
+    port: PORT
+  }
 });
