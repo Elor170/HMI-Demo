ba6c0bc8504e796c3dadd3d1e0975e5b30a2146c
Orche <orkv1234@gmail.com>
2024-09-11 16:35:11 +0300

    created basic map code

--- packages/hmi-frontend/src/Components/TopAppBar/TopAppBar.tsx
+++ packages/hmi-frontend/src/Components/TopAppBar/TopAppBar.tsx
@@ -5,14 +5,13 @@ import MenuButton from "./MenuButton";
 import { GAME_SERVER } from "@/Helper/consts";
 
 export default function TopAppBar() {
-
   return (
     <Box component={Paper}>
       <AppBar position="static" sx={{ display: "flex", flexDirection: "row" }}>
         <MenuButton nav="/">Home</MenuButton>
         <MenuButton nav="/streamer">Streamer</MenuButton>
         {/* <MenuButton nav="/waterfall">Waterfall</MenuButton> */}
-        {/* <MenuButton nav="/map">Map</MenuButton> */}
+        <MenuButton nav="/map">Map</MenuButton>
         <MenuButton nav="/io-checks">IO-Checks</MenuButton>
         {/* <MenuButton hrefVal={GAME_SERVER}>3D-Test</MenuButton> */}
 
--- packages/hmi-frontend/src/main.tsx
+++ packages/hmi-frontend/src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { defineCustomElements } from '@arcgis/map-components/dist/loader';
+
+defineCustomElements();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
