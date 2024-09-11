09550d4db18be6a790d81ccb940e88a0193539a5
Orche <orkv1234@gmail.com>
2024-09-11 16:35:11 +0300

    created basic map code

---
+++ packages/hmi-frontend/src/Pages/MapPage/MapPage.tsx
@@ -0,0 +1,44 @@
+import '@arcgis/core/assets/esri/themes/dark/main.css';
+import '@arcgis/map-components/dist/components/arcgis-map'
+import { ArcgisMap } from '@arcgis/map-components-react';
+
+
+import MapView from "@arcgis/core/views/MapView";
+import Map from "@arcgis/core/Map";
+import { useRef, useEffect } from "react";
+import Basemap from '@arcgis/core/Basemap';
+
+export default function MapPage() {
+
+  const mapDiv = useRef(null);
+
+  useEffect(() => {
+    const basemap = new Basemap({
+      portalItem: {
+        url: "https://192.168.253.231:6443/arcgis/rest/services/SampleWorldCities/MapServer"
+      }
+    })
+
+    // Basemap.fromJSON("https://192.168.253.231:6443/arcgis/rest/services/SampleWorldCities/MapServer")
+    if (mapDiv.current) {
+      /**
+       * Initialize application
+       */
+      const webmap = new Map({
+        basemap: basemap
+      });
+
+      const view = new MapView({
+        container: mapDiv.current, // The id or node representing the DOM element containing the view.
+        map: webmap, // An instance of a Map object to display in the view.
+        center: [-117.1490,32.7353],
+        scale: 10000000 // Represents the map scale at the center of the view.
+      });
+
+      return () => view && view.destroy()
+
+    }
+  }, []);
+
+  return <div className="mapDiv" ref={mapDiv} style={{height: '100vh', width: "100%"}}></div>;
+}
--- packages/hmi-frontend/src/main.tsx
+++ packages/hmi-frontend/src/main.tsx
@@ -5,6 +5,9 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import { defineCustomElements } from '@arcgis/map-components/dist/loader';
+
+defineCustomElements();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
