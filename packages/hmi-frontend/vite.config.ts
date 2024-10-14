fe0f2c81e295b5edbb11d7c187421dfd9dca30f5
Orche <orkv1234@gmail.com>
2024-10-14 12:47:21 +0300

    Fixed frontend build warnings
    Fixed warnings relating to code, and to scss

--- packages/hmi-frontend/vite.config.ts
+++ packages/hmi-frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 const PORT: number = process.env.PORT as unknown as number;
@@ -20,5 +20,12 @@ export default defineConfig({
   },
   build: {
     chunkSizeWarningLimit: Infinity,
-  }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
 });
