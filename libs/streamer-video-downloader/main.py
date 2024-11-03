7e7431b20a665577cb0fa7eb744ef601c09a2d60
Orche <orkv1234@gmail.com>
2024-11-03 12:30:25 +0200

    still doesn't work on youtube so moved to drive

--- libs/streamer-video-downloader/main.py
+++
@@ -1,6 +0,0 @@
-import pafy
-v = pafy.new("https://youtu.be/xp763iNB_MA?si=4C0K0IdSapeHKsim")
-s = v.getbest()
-print("Size is %s" % s.get_filesize())
-filename = s.download()  # starts download
-print(filename)
