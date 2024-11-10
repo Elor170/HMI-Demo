653ba20e6bf4a65fcbc659fef1ee26abbe1bbc88
Orche <orkv1234@gmail.com>
2024-11-10 10:24:23 +0200

    fix(pencil): Move default pot to 80

--- packages/streamer-logger/src/index.ts
+++ packages/streamer-logger/src/index.ts
@@ -1,17 +1,12 @@
 import express from "express";
 import logsRouter from "./routes/logger";
 import cors from "cors";
-import dotenv from "dotenv";
-dotenv.config();
-
-const { PORT } = process.env;
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/logs", logsRouter);
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+app.listen(80, () => {
+  console.log("Listening on port 80");
 });
